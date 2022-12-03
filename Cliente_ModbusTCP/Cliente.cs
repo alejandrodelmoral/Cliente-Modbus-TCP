0ae2243e978bbe97eca1a1089e0ec885a00e838e
alejandro.moral.lapresta <alejandro.moral.lapresta@alumnos.upm.es>
2022-12-03 19:36:22 +0100

    Subida de todo lo que llevo hasta ahora

---
+++ Cliente_ModbusTCP/Cliente.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cliente_ModbusTCP
+{
+    public class Cliente
+    {
+        private Socket cliente = null;
+        private IPEndPoint ipep = null;
+        private bool conectado = false;
+
+        public Cliente(string direccionIP, Int32 puerto)
+        {
+            try
+            {
+                IPAddress direccion = IPAddress.Parse(direccionIP);
+                ipep = new IPEndPoint(direccion, puerto);
+                cliente = new Socket(direccion.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                cliente.SendTimeout = 500;
+                cliente.ReceiveTimeout = 1000;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error general al preparar la conexión al servidor:\n" + ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+        }
+
+        public bool conectarServidor()
+        {
+            try
+            {
+                cliente.Connect(ipep);
+            }
+            catch (SocketException ex_sock)
+            {
+                if (ex_sock.SocketErrorCode == SocketError.ConnectionRefused)
+                    MessageBox.Show("Error al conectarse al servidor:\nAntes de poner en marcha el cliente, se debe poner en funcionamiento el servidor en el nodo:\nIP: " + ipep.Address.ToString() + " - Puerto: " + ipep.Port.ToString() + "\nAtención al \"firewall\"", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                    MessageBox.Show("Error al conectarse al servidor:\n" + ex_sock.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error general al conectarse al servidor:\n" + ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+            finally
+            {
+                if (cliente != null && cliente.Connected)
+                    conectado = true;
+                else
+                    conectado = false;
+            }
+
+            return conectado;
+        }
+
+        public void cierraCliente()
+        {
+            if (cliente != null && conectado)
+                cliente.Close();
+
+            cliente = null;
+            conectado = false;
+        }
+
+        public int enviaDatos(byte[] datos, int dim)
+        {
+            try
+            {
+                if (conectado)
+                {
+                    int res = cliente.Send(datos, dim, SocketFlags.None);
+
+                    if (res == dim)
+                        return res;
+                    else if (res == 0)
+                    {
+                        cliente = null;
+                        conectado = false;
+                        return -1;
+                    }
+                    else
+                        return -2;
+                }
+                else
+                    return -1;
+            }
+            catch (SocketException ex_sock)
+            {
+                if (ex_sock.SocketErrorCode == SocketError.TimedOut)
+                    return 0;
+                else if (ex_sock.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    cliente = null;
+                    conectado = false;
+                    return -1;
+                }
+                else
+                    return -2;
+            }
+            catch (Exception ex)
+            {
+                return -2;
+            }
+        }
+
+        public int recibeDatos(byte[] datos, int dimMax)
+        {
+            try
+            {
+                if (conectado)
+                {
+                    int res = cliente.Receive(datos, dimMax, SocketFlags.None);
+
+                    if (res > 0)
+                        return res;
+                    else if (res == 0)
+                    {
+                        cliente = null;
+                        conectado = false;
+                        return -1;
+                    }
+                    else
+                        return -2;
+                }
+                else
+                    return -1;
+            }
+            catch (SocketException ex_sock)
+            {
+                if (ex_sock.SocketErrorCode == SocketError.TimedOut)
+                    return 0;
+                else if (ex_sock.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    cliente = null;
+                    conectado = false;
+                    return -1;
+                }
+                else
+                    return -2;
+            }
+            catch (Exception ex)
+            {
+                return -2;
+            }
+        }
+
+    }
+}
---
+++ Cliente_ModbusTCP/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente_ModbusTCP
+{
+    public partial class MainWindow : Window
+    {
+        Cliente cliente = null;
+        bool conectado = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /*-- Menú Dispositivos --*/
+        private void Yo_Click(object sender, RoutedEventArgs e)
+        {
+            lb_Dispositivo.Visibility = Visibility.Visible;
+            lb_Nombre.Content = "Yo";
+            tb_DireccionIP.Text = "127.0.0.1";
+            tb_DireccionIP.IsEnabled = false;
+            tb_Puerto.Text = "502";
+            tb_Puerto.IsEnabled = false;
+            return;
+        }
+
+        private void PC_13_Click(object sender, RoutedEventArgs e)
+        {
+            lb_Dispositivo.Visibility = Visibility.Visible;
+            lb_Nombre.Content = "PC 13 - Lab. 019";
+            tb_DireccionIP.Text = "10.172.19.13";
+            tb_DireccionIP.IsEnabled = false;
+            tb_Puerto.Text = "502";
+            tb_Puerto.IsEnabled = false;
+            return;
+        }
+
+        private void Borrar_Click(object sender, RoutedEventArgs e)
+        {
+            lb_Dispositivo.Visibility = Visibility.Hidden;
+            lb_Nombre.Content = "";
+            tb_DireccionIP.Text = "";
+            tb_DireccionIP.IsEnabled = true;
+            tb_Puerto.Text = "";
+            tb_Puerto.IsEnabled = true;
+            return;
+        }
+        /*-----------------------*/
+
+        /*-- Menú Ayuda --*/
+        private void Sobre_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /*----------------*/
+        
+        /*-- Botones --*/
+        private void btn_Conectar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!conectado)
+            {
+                cliente = new Cliente(tb_DireccionIP.Text, Convert.ToInt32(tb_Puerto.Text));
+
+                if (cliente.conectarServidor())
+                {
+                    btn_Conectar.Content = "Desconectar";
+                    btn_Peticion.IsEnabled = true;
+                    Title = "Cliente Modbus/TCP (TLS) - Conectado";
+                    conectado = true;
+                }
+                else
+                    cliente = null;
+            }
+            else
+            {
+                if (cliente != null)
+                    cliente.cierraCliente();
+
+                cliente = null;
+                conectado = false;
+                btn_Conectar.Content = "Conectar";
+                Title = "Cliente Modbus/TCP (TLS)";
+                btn_Peticion.IsEnabled = false;
+            }
+
+            return;
+        }
+
+        private void btn_Peticion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            if (cliente != null)
+                cliente.cierraCliente();
+
+            cliente = null;
+            Close();
+            return;
+        }
+        /*-------------*/
+
+    }
+}
